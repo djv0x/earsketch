c1f291af103e227f401703cc3369192f2fb7dbf6
djv0x <35152330+djv0x@users.noreply.github.com>
2018-12-03 08:29:38 -0500

    Add files via upload

---
+++ JS3_MiniTask_FixtheErrors_SampleScript.js
@@ -0,0 +1,48 @@
+// javascript code
+//
+// script_name: MiniTask - Debugging
+//
+// author:
+//
+// description: Fix the errors in this
+// piece of code and use a comment statement 
+// to document how you fixed the errors
+//
+
+init(); 
+setTempo(120); 
+
+//Music
+
+drums = RD_EDM_DRUMBEATPART_5;
+drums2 = RD_EDM_MAINBEAT_9;
+drums4 = YG_EDM_KICK_3;
+chords = RD_EDM_CHORDPART_1;
+lead = RD_EDM_RAVELEAD_5;
+bass = ELECTRO_ANALOGUE_BASS_013;
+claps = YG_EDM_CLAP_4;
+noiseSweep = TECHNO_WHITENOISESFX_001;
+
+//Leads
+
+fitMedia(lead, 4, 11, 13); //Try commenting out this block first
+
+
+//Beat
+
+fitMedia(drums, 2, 1, 6); //Try commenting out this block next
+fitMedia(drums, 2, 7, 15;
+fitMedia(claps, 3, 6, 7);
+fitMedia(drums2, 7, 7, 15);
+fitMedia(drums3, 7, 3, 6);
+
+//Auxiliary
+
+fitMedia(chords, 3, 7, 17);
+fitMedia(noiseSweep, 6, 5, 7);
+fitMedia(bass, 5, 7, 17);
+
+//Finish
+
+finish();
+
